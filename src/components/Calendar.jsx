9d1040b51b06eca4cacb9e6744bebf6c5eb9ab37
Eduardo Maldonado <emaldonadon1@gmail.com>
2023-05-29 18:05:57 -0500

    updated

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,13 +5,61 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import esLocale from '@fullcalendar/core/locales/es'
 
+const event = [
+  {
+    title: 'Hora libre',
+    start: '2023-05-30T10:00:00',
+    end: '2023-05-30T12:00:00',
+  },
+  {
+    title: 'Hora de descanso',
+    start: '2023-06-01T14:30:00',
+    end: '2023-06-01T16:30:00',
+  },
+  {
+    title: 'Hora libre',
+    start: '2023-05-30T10:00:00',
+    end: '2023-05-30T12:00:00',
+  },
+  {
+    title: 'Hora libre',
+    start: '2023-05-30T10:00:00',
+    end: '2023-05-30T12:00:00',
+  },
+  {
+    title: 'Hora libre',
+    start: '2023-05-30T10:00:00',
+    end: '2023-05-30T12:00:00',
+  },
+  {
+    title: 'Hora libre',
+    start: '2023-05-30T10:00:00',
+    end: '2023-05-30T12:00:00',
+  },
+  {
+    title: 'Hora libre',
+    start: '2023-05-30T10:00:00',
+    end: '2023-05-30T12:00:00',
+  },
+];
+
 function Calendar() {
+  
+
   return (
     <div>
-      <Fullcalendar 
-      locale={esLocale}
+      <Fullcalendar
+        locale={esLocale}
+      
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"dayGridMonth"}
+        weekends={true}
+        events={event}
+        /* select={handleSelect}*/
+         editable={false}  
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
         headerToolbar={{
           left: "prev,next today",
           center: "title",
@@ -23,5 +71,6 @@ function Calendar() {
     </div>
   );
 }
+
 
 export default Calendar;
